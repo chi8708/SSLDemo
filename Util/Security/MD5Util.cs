91edfc7b7fdadd27ec7ff39e5816def0b9ca42d4
chiDreal <chitongshang@outlook.com>
2024-02-22 11:31:37 +0800

    添加加密

---
+++ Util/Security/MD5Util.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Security
+{
+    internal class MD5Util
+    {
+        /// <summary>
+        /// MD5算法
+        /// </summary>
+        /// <param name="encypStr"></param>
+        /// <param name="charset"></param>
+        /// <returns></returns>
+        public static string Md5_BitConverter(string input)
+        {
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+
+
+        public static string Md5_StringBuilder(string input)
+        {
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+            foreach (var hashByte in hashBytes)
+            {
+                sb.Append(hashByte.ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Util/Security/RsaUtil.cs
@@ -0,0 +1,36 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Security
+{
+    internal class RsaUtil
+    {
+        /// <summary>
+        /// RSA 加密
+        /// </summary>
+        /// <param name="sSource" >Source string</param>
+        /// <param name="sPublicKey" >public key</param>
+        /// <returns></returns>
+        public static string RsaEncryptString(string sSource, string publicKeyString)
+        {
+            byte[] publicKeyBytes = Convert.FromBase64String(publicKeyString);
+            AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(publicKeyBytes);
+            RsaKeyParameters rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
+            RSAParameters rsaParameters = new RSAParameters();
+            rsaParameters.Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned();
+            rsaParameters.Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned();
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(rsaParameters);
+
+            var cipherbytes = rsa.Encrypt(Encoding.UTF8.GetBytes(sSource), false);
+            return Convert.ToBase64String(cipherbytes);
+        }
+    }
+}
